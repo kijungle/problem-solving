9d1e4ab0016ac2b80614e8ea5c28cc3249b90a88
Kibalza <doo3002@gmail.com>
2022-11-23 18:07:06 +0900

    [BOJ] 1182 부분수열의 합 S2 Brute-force/BT

---
+++ BOJ1182.py
@@ -0,0 +1,21 @@
+#221123 1753~1806
+#Silver 2 Brute-force & Recursive
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(100000)
+n, s = map(int, input().split())
+permutation = list(map(int, input().split()))
+ans = 0
+def backTracking(idx, _sum):
+    global ans, s
+    if idx >= n:
+        return
+    _sum += permutation[idx]
+    if _sum == s:
+        ans += 1
+        
+    backTracking(idx+1, _sum)
+    backTracking(idx+1, _sum-permutation[idx])
+
+backTracking(0,0)
+print(ans)
